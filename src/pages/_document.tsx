1fca380f35eacf286029e80dcb9a2605b3cfb2c2
Alberto <albertovinicius3@gmail.com>
2022-11-21 14:57:42 -0300

    feat(_document): change Roboto to Poppins fontFamily

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
                     crossOrigin="anonymous"
                 />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap"
                     rel="stylesheet"
                 />
             </Head>
