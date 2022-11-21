229278111447cfdd1d6c31a94a22cf28798e1868
Alberto <albertovinicius3@gmail.com>
2022-11-21 15:02:22 -0300

    feat(components): add Container component

---
+++ src/components/Container.tsx
@@ -0,0 +1,25 @@
+import { FunctionComponent, HTMLAttributes, ReactNode } from "react";
+import { classNames } from "../utils";
+
+interface ContainerProps extends HTMLAttributes<HTMLDivElement> {
+    children: ReactNode;
+}
+
+const Container: FunctionComponent<ContainerProps> = ({
+    children,
+    ...props
+}) => {
+    return (
+        <div
+            {...props}
+            className={classNames(
+                props.className ? props.className : "",
+                "container px-4 mx-auto"
+            )}
+        >
+            {children}
+        </div>
+    );
+};
+
+export default Container;
