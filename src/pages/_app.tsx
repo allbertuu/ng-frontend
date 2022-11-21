a26ff0d5f94c8cfb685a5c8ec0ceebfc7d3b3a60
Alberto <albertovinicius3@gmail.com>
2022-11-21 14:58:51 -0300

    feat(_app): add AuthProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app";
+import { AuthProvider } from "../contexts/AuthContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <AuthProvider>
+            <Component {...pageProps} />
+        </AuthProvider>
+    );
 }
