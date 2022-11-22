8c9b1dcd4951ce2fab5026979a9d75df906ebdbe
Alberto <albertovinicius3@gmail.com>
2022-11-22 17:13:12 -0300

    refactor: refactor AuthContext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,48 +1,48 @@
 import { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
 import { setCookie, destroyCookie } from 'nookies';
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode } from 'react';
 import api from '../services/api';
 
 interface IAuthProvider {
     children: ReactNode;
 }
 
-interface IUser {
+export interface IUserAccount {
+    accountId: string;
+    balance: number;
+}
+
+interface ISignInCredentials {
     username: string;
     password: string;
 }
 
-interface ISignInCredentials extends IUser {}
-
 interface IAuthContext {
     signIn(credentials: ISignInCredentials): Promise<void>;
     signOut(): void;
-    isAuthenticated: boolean;
-    user: IUser | null;
 }
 
 export const AuthContext = createContext({} as IAuthContext);
 
 export function AuthProvider({ children }: IAuthProvider) {
-    const [user, setUser] = useState<IUser | null>(null);
     const router = useRouter();
-    const isAuthenticated = !!user;
 
     const signIn = async ({ username, password }: ISignInCredentials) => {
         const p = new Promise<void>(async (resolve, reject) => {
             try {
-                const res = await api.post('session', {
+                // pegar o token do usuário
+                const sessionResponse = await api.post('session', {
                     username,
                     password,
                 });
-                const { token } = res.data;
+                const { token } = sessionResponse.data;
+                // salvar o cookie (Token JWT) no browser
                 setCookie(undefined, 'ngbackend.token', token, {
                     maxAge: 60 * 60 * 24 * 30, // 30 days
                     path: '/',
                 });
-                setUser({ username, password });
-                api.defaults.headers['Authorization'] = `Bearer ${token}`;
+                // redirecionar para home
                 router.push('/home');
                 resolve();
             } catch (e) {
@@ -60,7 +60,7 @@ export function AuthProvider({ children }: IAuthProvider) {
 
     return (
         <AuthContext.Provider
-            value={{ signIn, signOut, isAuthenticated }}
+            value={{ signIn, signOut }}
         >
             {children}
         </AuthContext.Provider>
